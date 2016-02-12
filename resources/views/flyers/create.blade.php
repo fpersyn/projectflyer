9170f50dce0b62ff371efeba577599cf225d9be5
Frédéric Persyn <frederic.persyn@gmail.com>
2016-02-12 14:04:55 +0000

    added form to the create view

--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -2,4 +2,61 @@
 
 @section('content')
     <h1>Selling Your Home?</h1>
+
+    <hr>
+
+    <form method="post" action="/flyers">
+        <!-- Street Form Input -->
+        <div class="form-group">
+            <label for="street">Street:</label>
+            <input type="text" name="street" id="street" class="form-control" value="{{ old('street') }}">
+        </div>
+
+        <!-- City Form Input -->
+        <div class="form-group">
+            <label for="city">City:</label>
+            <input type="text" name="city" id="city" class="form-control" value="{{ old('city') }}">
+        </div>
+
+        <!-- Zip Form Input -->
+        <div class="form-group">
+            <label for="zip">Zip/Postal Code:</label>
+            <input type="text" name="zip" id="zip" class="form-control" value="{{ old('zip') }}">
+        </div>
+
+        <!-- Country Form Input -->
+        <div class="form-group">
+            <label for="country">Country:</label>
+            <select id="country" name="country" class="form-control">
+            </select>
+        </div>
+
+        <!-- State Form Input -->
+        <div class="form-group">
+            <label for="state">State:</label>
+            <select id="state" name="state" class="form-control">
+            </select>
+        </div>
+
+        <hr>
+
+        <!-- Price Form Input -->
+        <div class="form-group">
+            <label for="price">Sale Price:</label>
+            <input type="text" name="price" id="price" class="form-control" value="{{ old('price') }}">
+        </div>
+
+        <!-- Description Form Input -->
+        <div class="form-group">
+            <label for="description">Home Description:</label>
+            <textarea type="text" name="description" id="description" class="form-control" rows="10">
+                {{ old('description') }}
+            </textarea>
+        </div>
+
+        <!-- Form Submit -->
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary">Create Flyer</button>
+        </div>
+    </form>
 @stop
