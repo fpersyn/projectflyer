008dddd86d5823ca7c6d58c1df187172dbc5724d
Frédéric Persyn <frederic.persyn@gmail.com>
2016-02-13 16:53:20 +0000

    set up the relationship between a flyer and a photo

---
+++ app/Photo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Photo extends Model
+{
+
+    // Table name override
+    // by default Laravel will try to use the pluralised class name as the table name
+    protected $table = 'flyer_photos';
+
+    protected $fillable = ['photo'];
+
+    /**
+     * A photo belongs to a flyer
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function flyer()
+    {
+        return $this->belongsTo('App\Flyer');
+    }
+}
