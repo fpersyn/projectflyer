17d0fb6b3e5377c231871643f4b65a118ba9fe87
Frédéric Persyn <frederic.persyn@gmail.com>
2016-02-13 16:00:30 +0000

    moved flyer form fields into partial

---
+++ resources/views/flyers/form.blade.php
@@ -0,0 +1,63 @@
+@inject('countries', 'App\Http\Utilities\Country')
+
+<!-- Street Form Input -->
+<div class="form-group">
+    <label for="street">Street:</label>
+    <input type="text" name="street" id="street" class="form-control" value="{{ old('street') }}">
+</div>
+
+<!-- City Form Input -->
+<div class="form-group">
+    <label for="city">City:</label>
+    <input type="text" name="city" id="city" class="form-control" value="{{ old('city') }}">
+</div>
+
+<!-- Zip Form Input -->
+<div class="form-group">
+    <label for="zip">Zip/Postal Code:</label>
+    <input type="text" name="zip" id="zip" class="form-control" value="{{ old('zip') }}">
+</div>
+
+<!-- Country Form Input -->
+<div class="form-group">
+    <label for="country">Country:</label>
+    <select id="country" name="country" class="form-control">
+        @foreach($countries::all() as $country => $code)
+            <option value="{{ $code }}">{{ $country }}</option>
+        @endforeach
+    </select>
+</div>
+
+<!-- State Form Input -->
+<div class="form-group">
+    <label for="state">State:</label>
+    <select id="state" name="state" class="form-control">
+    </select>
+</div>
+
+<hr>
+
+<!-- Price Form Input -->
+<div class="form-group">
+    <label for="price">Sale Price:</label>
+    <input type="text" name="price" id="price" class="form-control" value="{{ old('price') }}">
+</div>
+
+<!-- Description Form Input -->
+<div class="form-group">
+    <label for="description">Home Description:</label>
+            <textarea type="text" name="description" id="description" class="form-control" rows="10">
+                {{ old('description') }}
+            </textarea>
+</div>
+
+<!-- Photos Form Input -->
+<div class="form-group">
+    <label for="photos">Photos:</label>
+    <input type="file" name="photos" id="photos" class="form-control" value="{{ old('photos') }}">
+</div>
+
+<!-- Form Submit -->
+<div class="form-group">
+    <button type="submit" class="btn btn-primary">Create Flyer</button>
+</div>
